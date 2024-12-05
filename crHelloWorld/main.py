52c91ce75c235343471af925c2d90b447bcd7d13
Martin Kowalczyk <martinkowalczyk@Martins-MacBook-Pro.local>
2024-12-05 13:15:52 -0600

    first commit cr testing CD in GCP

---
+++ crHelloWorld/main.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, Cloud Run!"
+
+if __name__ == "__main__":
+    # Listen on PORT (required for Cloud Run)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
